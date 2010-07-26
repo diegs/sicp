0420a5916303104d8f9cc943ae83fe535e09113b
Diego Pontoriero <dpontor@gmail.com>
2010-07-26 01:44:44 -0700

    a couple of more exercises in 2.3. it's tough to get through.

--- chapter2.3.scm
+++ chapter2.3.scm
@@ -409,3 +409,113 @@
 	((< given-key (key (entry set-of-records)))
 	 (lookup given-key (left-tree set-of-records)))
 	(else (lookup given-key (right-tree set-of-records)))))
+
+;2.3.4 Example: Huffman Encoding Trees
+(define (make-leaf symbol weight) (list 'leaf symbol weight))
+
+(define (leaf? object) (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+	right
+	(append (symbols left) (symbols right))
+	(+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+	'()
+	(let ((next-branch
+	       (choose-branch (car bits) current-branch)))
+	  (if (leaf? next-branch)
+	      (cons (symbol-leaf next-branch)
+		    (decode-1 (cdr bits) tree))
+	      (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+	((= bit 1) (right-branch branch))
+	(else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+	((< (weight x) (weight (car set))) (cons x set))
+	(else (cons (car set)
+		    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+	(adjoin-set (make-leaf (car pair)   ; symbol
+			       (cadr pair)) ; frequency
+		    (make-leaf-set (cdr pairs))))))
+
+;Exercise 2.67: Define an encoding tree and a sample message
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+		  (make-code-tree
+		   (make-leaf 'B 2)
+		   (make-code-tree (make-leaf 'D 1)
+				   (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(decode sample-message sample-tree)
+;Value 52: (a d a b b c a)
+
+;*Exercise 2.68:* The `encode' procedure takes as arguments a
+;message and a tree and produces the list of bits that gives the
+;encoded message.
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+	      (encode (cdr message) tree))))
+
+(define (contains? elem list)
+  (cond ((null? list) #f)
+	((eq? elem (car list)) #t)
+	(else (contains elem (cdr list)))))
+
+(define (encode-symbol symbol tree)
+  (cond ((leaf? tree) (if (eq? symbol (symbol-leaf tree)) '()
+			    (error "symbol not in tree!")))
+	  ((contains? symbol (symbols (left-branch tree)))
+	   (cons 0 (encode-symbol symbol (left-branch tree))))
+	  ((contains? symbol (symbols (right-branch tree)))
+	   (cons 1 (encode-symbol symbol (right-branch tree))))
+	  (else (error "symbol not in tree!"))))
+
+(encode '(a d a b b c a) sample-tree)
+;Value 53: (0 1 1 0 0 1 0 1 0 1 1 1 0)
+
+;; *Exercise 2.69:* The following procedure takes as its argument a
+;; list of symbol-frequency pairs (where no symbol appears in more
+;; than one pair) and generates a Huffman encoding tree according to
+;; the Huffman algorithm.
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge leaf-set))
